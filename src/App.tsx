112e21424c273745505ec3d970bcf906997d6802
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-10-30 09:19:59 -0300

    feat(ui): Implements loaded file state in InputSingleFile The `InputSingleFile` component has been updated to display different interfaces based on selecting a file. - When no file is selected, the default upload area is shown, allowing the user to select or drag a file. - After selection, the component displays the name of the loaded file, a representative icon and a "Remove" button. - The "Remove" button clears the field value on the form, returning the component to its initial state.

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,11 @@ import InputText from "./components/input-text";
 import SearchIcon from "./assets/icons/search.svg?react";
 import InputCheckbox from "./components/input-checkbox";
 import InputSingleFile from "./components/input-single-file";
+import { useForm } from "react-hook-form";
 
 export default function App() {
+  const form = useForm();
+
   return (
     <div className="grid gap-7 p-6">
       <div className="flex gap-3">
@@ -57,13 +60,13 @@ export default function App() {
         <InputText icon={SearchIcon} placeholder="Buscar foto" />
       </div>
 
-			<div>
-				<InputCheckbox/>
-			</div>
+      <div>
+        <InputCheckbox />
+      </div>
 
-			<div>
-				<InputSingleFile/>
-			</div>
+      <div>
+        <InputSingleFile form={form} {...form.register("file")} />
+      </div>
     </div>
   );
 }
--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -4,6 +4,8 @@ import Text, { textVariants } from "./text";
 import UploadFileIcon from "../assets/icons/upload-file.svg?react";
 import type React from "react";
 import FileImageIcon from "../assets/icons/image.svg?react";
+import { useWatch } from "react-hook-form";
+import { useMemo } from "react";
 
 export const inputSingleFileVariants = tv({
   base: `
@@ -37,63 +39,86 @@ interface InputSingleFileProps
   extends VariantProps<typeof inputSingleFileVariants>,
     Omit<React.ComponentProps<"input">, "size"> {
   error?: React.ReactNode;
+  form: any;
 }
 
-export default function InputSingleFile({ size, error }: InputSingleFileProps) {
+export default function InputSingleFile({
+  size,
+  error,
+  form,
+  ...props
+}: InputSingleFileProps) {
+  const formValues = useWatch({ control: form.control }); //consegue ver quais valores são alterados no form
+  const name = props.name || "";
+  //o arquivo do formulário
+  const formFile: File = useMemo(
+    () => formValues[name]?.[0],
+    [formValues, name]
+  );
+
   return (
     <div>
-      <div className="w-full relative group cursor-pointer">
-        <input
-          type="file"
-          className={`absolute top-0 right-0 w-full h-full opacity-0
+      {!formFile ? (
+        //parte que é exibida quando não há arquivo - quando não foi carregado
+        <>
+          <div className="w-full relative group cursor-pointer">
+            <input
+              type="file"
+              className={`absolute top-0 right-0 w-full h-full opacity-0
           cursor-pointer`}
-        />
-        <div className={inputSingleFileVariants({ size })}>
-          <Icon
-            svg={UploadFileIcon}
-            className={inputSingleFileIconVariants({ size })}
-          />
-          <Text
-            variant="label-medium"
-            className={`text-placeholder text-center`}
-          >
-            Arraste o arquivo arquivo
-            <br />
-            ou clique aqui para selecionar
-          </Text>
-        </div>
-      </div>
-      {error && (
-        <Text variant="label-small" className="text-accent-red">
-          Erro no campo
-        </Text>
-      )}
-      <div
-        className={`
+              {...props}
+            />
+            <div className={inputSingleFileVariants({ size })}>
+              <Icon
+                svg={UploadFileIcon}
+                className={inputSingleFileIconVariants({ size })}
+              />
+              <Text
+                variant="label-medium"
+                className={`text-placeholder text-center`}
+              >
+                Arraste o arquivo arquivo
+                <br />
+                ou clique aqui para selecionar
+              </Text>
+            </div>
+          </div>
+          {error && (
+            <Text variant="label-small" className="text-accent-red">
+              Erro no campo
+            </Text>
+          )}
+        </>
+      ) : (
+        //parte que é exibida quando há arquivo - quando foi carregado
+        <div
+          className={`
         flex gap-3 items-center border border-solid border-border-primary 
         mt-5 p-3 rounded
         `}
-      >
-        <Icon svg={FileImageIcon} className="fill-white w-6 h-6" />
-        <div className="flex flex-col">
-          <div className="truncate max-w-80">
-            <Text variant="label-medium" className="text-placeholder">
-              Nome do arquivo.png
-            </Text>
-          </div>
-          <div className="flex">
-            <button
-              type="button"
-              className={textVariants({
-                variant: "label-small",
-                className: "text-accent-red cursor-pointer hover:underline",
-              })}
-            >
-              Remover
-            </button>
+        >
+          <Icon svg={FileImageIcon} className="fill-white w-6 h-6" />
+          <div className="flex flex-col">
+            <div className="truncate max-w-80">
+              <Text variant="label-medium" className="text-placeholder">
+                {formFile.name}
+              </Text>
+            </div>
+            <div className="flex">
+              <button
+                type="button"
+                className={textVariants({
+                  variant: "label-small",
+                  className: "text-accent-red cursor-pointer hover:underline",
+                })}
+                onClick={() => form.setValue(name, undefined)} //remove o arquivo
+              >
+                Remover
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
