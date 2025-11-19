b3f4fe9956cb94358f707fde6b56d0d8d8ae4a2c
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-11-19 16:54:04 -0300

    feat(photos): Adds navigation to the photo details page Creates the new `PhotosNavigator` component to allow the user to navigate to the previous and next photo directly from the details page. - The `PhotosNavigator` component includes "previous" and "next" buttons. - Adds a loading state with `Skeleton` for the navigation buttons. - Integrates the new component into the `PagePhotoDetails` page.

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,15 +1,29 @@
+import Container from "../components/container";
 import Text from "../components/text";
 import { useParams } from "react-router";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 
 export default function PagePhotoDetails() {
   //para capturar o :id da rota
   const { id } = useParams();
 
+  //apenas para fazer o test do mock
+  const isLoadingPhoto = false;
+  const photo = {} as Photo;
+
   return (
-    <>
-      <Text variant="heading-medium">Página detalhe da foto</Text>
-      <hr />
-      <Text variant="heading-medium">Id da foto: {id}</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator />
+      </header>
+    </Container>
   );
 }
