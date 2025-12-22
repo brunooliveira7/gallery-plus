3e4150bfbe27b630f53aef391797c0e452ff907f
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-12-22 10:14:21 -0300

    feat(albums): implement album creation form with validation schema

--- src/contexts/albums/components/album-new-dialog.tsx
+++ src/contexts/albums/components/album-new-dialog.tsx
@@ -14,13 +14,31 @@ import SelectCheckboxIllustration from "../../../assets/images/select-checkbox.s
 import Skeleton from "../../../components/skeleton";
 import PhotoImageSelectable from "../../photos/components/photo-image-selectable";
 import usePhotos from "../../photos/hooks/use-photos";
+import { useForm } from "react-hook-form";
+import { albumNewFormSchema, type AlbumNewFormSchema } from "../schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
 
 interface AlbumNewDialogProps {
   trigger: React.ReactNode;
 }
 
 export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
+  //para reset dos campos do modal
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const form = useForm<AlbumNewFormSchema>({
+    resolver: zodResolver(albumNewFormSchema),
+  });
+
   const { photos, isLoadingPhotos } = usePhotos();
+
+  //reset dos campos do modal
+  useEffect(() => {
+    if (!modalOpen) {
+      form.reset();
+    }
+  }, [modalOpen, form]);
 
   function handleTogglePhoto(selected: boolean, photoId: string) {
     console.log(
@@ -29,67 +47,79 @@ export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
     );
   }
 
+  function handleSubmit(payload: AlbumNewFormSchema) {
+    console.log(payload);
+  }
+
   return (
-    <Dialog>
+    <Dialog open={modalOpen} onOpenChange={setModalOpen}>
       {/*abrir o dialog - asChild - não criar o botão, renderiza o componente */}
       <DialogTrigger asChild>{trigger}</DialogTrigger>
 
       <DialogContent>
-        <DialogHeader>Criar álbum</DialogHeader>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <DialogHeader>Criar álbum</DialogHeader>
 
-        <DialogBody className="flex flex-col gap-5">
-          <InputText placeholder="Adicione um título" />
+          <DialogBody className="flex flex-col gap-5">
+            <InputText
+              placeholder="Adicione um título"
+              error={form.formState.errors.title?.message}
+              {...form.register("title")}
+            />
 
-          <div className="space-y-3">
-            <Text as="div" variant="label-small">
-              Fotos cadastradas
-            </Text>
+            <div className="space-y-3">
+              <Text as="div" variant="label-small">
+                Fotos cadastradas
+              </Text>
 
-            {!isLoadingPhotos && photos?.length > 0 && (
-              <div className="flex flex-wrap gap-2">
-                {photos.map((photo) => (
-                  <PhotoImageSelectable
-                    key={photo.id}
-                    src={`${import.meta.env.VITE_IMAGES_URL}/${photo.imageId}`}
-                    title={photo.title}
-                    imageClassName="w-20 h-20"
-                    onSelectImage={(selected) =>
-                      handleTogglePhoto(selected, photo.id)
-                    }
-                  />
-                ))}
-              </div>
-            )}
+              {!isLoadingPhotos && photos?.length > 0 && (
+                <div className="flex flex-wrap gap-2">
+                  {photos.map((photo) => (
+                    <PhotoImageSelectable
+                      key={photo.id}
+                      src={`${import.meta.env.VITE_IMAGES_URL}/${
+                        photo.imageId
+                      }`}
+                      title={photo.title}
+                      imageClassName="w-20 h-20"
+                      onSelectImage={(selected) =>
+                        handleTogglePhoto(selected, photo.id)
+                      }
+                    />
+                  ))}
+                </div>
+              )}
 
-            {isLoadingPhotos && (
-              <div className="flex flex-wrap gap-2">
-                {Array.from({ length: 4 }).map((_, index) => (
-                  <Skeleton
-                    key={`photo-loading-${index}`}
-                    className="w-20 h-20 rounded-lg"
-                  />
-                ))}
-              </div>
-            )}
+              {isLoadingPhotos && (
+                <div className="flex flex-wrap gap-2">
+                  {Array.from({ length: 4 }).map((_, index) => (
+                    <Skeleton
+                      key={`photo-loading-${index}`}
+                      className="w-20 h-20 rounded-lg"
+                    />
+                  ))}
+                </div>
+              )}
 
-            {!isLoadingPhotos && photos?.length === 0 && (
-              <div className="w-full flex flex-col justify-center items-center gap-3">
-                <SelectCheckboxIllustration />
-                <Text variant="paragraph-large" className="text-center">
-                  Nenhuma foto disponível para seleção.
-                </Text>
-              </div>
-            )}
-          </div>
-        </DialogBody>
+              {!isLoadingPhotos && photos?.length === 0 && (
+                <div className="w-full flex flex-col justify-center items-center gap-3">
+                  <SelectCheckboxIllustration />
+                  <Text variant="paragraph-large" className="text-center">
+                    Nenhuma foto disponível para seleção.
+                  </Text>
+                </div>
+              )}
+            </div>
+          </DialogBody>
 
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="secondary">Cancelar</Button>
-          </DialogClose>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="secondary">Cancelar</Button>
+            </DialogClose>
 
-          <Button>Criar</Button>
-        </DialogFooter>
+            <Button type="submit">Criar</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
